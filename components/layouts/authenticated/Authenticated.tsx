6f157125f36c5f81bd23f958744b2b73eadcc148
Leon Chux <epicgenii18@gmail.com>
2022-11-08 14:19:01 +0100

    setup route authorization

--- components/layouts/authenticated/Authenticated.tsx
+++ components/layouts/authenticated/Authenticated.tsx
@@ -1,6 +1,8 @@
 import Sidebar from 'components/navigation/sidebar/SideBar';
 import Display from 'components/navigation/display/Display';
+import Unauthorized from 'components/utility/unauthorized/Unauthorized';
 import ToastWrapper from 'components/utility/toast_wrapper/ToastWrapper';
+import { useSelector } from 'react-redux';
 
 import type { NextPage } from 'next';
 
@@ -9,6 +11,18 @@ type Props = {
 };
 
 const Authenticated: NextPage<Props> = ({ children }) => {
+  const loginToken = useSelector(
+    (state: any) => state.authentication.loginToken
+  );
+
+  if (!loginToken) {
+    return (
+      <div>
+        <Unauthorized />
+      </div>
+    );
+  }
+
   return (
     <div className='container mx-auto relative'>
       <Sidebar />
---
+++ components/utility/unauthorized/Unauthorized.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import scarecrowImage from 'images/scarecrow.png';
+import Link from 'next/link';
+
+const Unauthorized: NextPage = () => {
+  return (
+    <div className='!min-h-screen flex lg:flex-row flex-col lg:justify-between justify-center'>
+      <div className='w-full lg:w-3/5 xl:w-2/5 flex-col flex justify-center md:items-center '>
+        <div className='pl-2 pr-2'>
+          <h2 className='text-4xl mb-2'>Unauthorized Access</h2>
+          <p>Looks like you are currently logged out.</p>
+          <p className='mb-2'>
+            Login as your preferred user below and try again.
+          </p>
+          <div className='flex gap-x-2 md:gap-x-4'>
+            <Link href='/resident'>
+              <a className='bg-gray-600 hover:bg-black text-white pl-4 pr-4 rounded-lg pb-1 pt-1'>
+                Resident
+              </a>
+            </Link>
+            <Link href='#'>
+              <a className='bg-gray-600 hover:bg-black text-white pl-4 pr-4 rounded-lg pb-1 pt-1'>
+                Security
+              </a>
+            </Link>
+            <Link href='#'>
+              <a className='bg-gray-600 hover:bg-black text-white pl-4 pr-4 rounded-lg pb-1 pt-1'>
+                Admin
+              </a>
+            </Link>
+            <Link href='#'>
+              <a className='bg-gray-600 hover:bg-black text-white pl-4 pr-4 rounded-lg pb-1 pt-1'>
+                SuperAdmin
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='w-full lg:w-3/5  flex items-center flex-col justify-center'>
+        <Image alt='' className='w-full h-auto  ' src={scarecrowImage} />
+      </div>
+    </div>
+  );
+};
+export default Unauthorized;
