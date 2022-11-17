da71312da0afad8824bcae5cd8a15c0c86c98d79
Leon Chux <epicgenii18@gmail.com>
2022-11-17 20:55:41 +0100

    worked on mobile bottom menu

---
+++ components/navigation/mobile_menu/MobileMenu.tsx
@@ -0,0 +1,124 @@
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateLoginData } from 'reducers/authentication';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHouse,
+  faUsers,
+  faClipboardList,
+  faTruckMedical,
+  faUser,
+  faRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+
+import Link from 'next/link';
+const MobileMenu: NextPage = () => {
+  const router = useRouter();
+  const updateLoginDataDispatch = useDispatch();
+  const selectedRouteStyle = ' text-digiDefault ';
+
+  return (
+    <div className='bg-gray-600 lg:hidden fixed bottom-0 left-0  w-screen ml-auto mr-auto'>
+      <div className=' xl:w-1/2 items-stretch   flex justify-between text-sm ml-auto mr-auto '>
+        <div
+          className={`${
+            router.pathname == '/app' ? selectedRouteStyle : 'text-gray-400'
+          }    text-gray-400 pt-4  w-1/4  text-center pb-4 cursor-pointer transition-all duration-700 hover:bg-gray-600  hover:text-digiDefault`}
+        >
+          <Link href='/app'>
+            <a className=' block text-center'>
+              <FontAwesomeIcon className={` mr-2  `} icon={faHouse} />
+              <span className='block'>Home</span>
+            </a>
+          </Link>
+        </div>
+        <div
+          className={`${
+            router.pathname.includes('/app/dependents')
+              ? selectedRouteStyle
+              : 'text-gray-400'
+          }   pt-4  w-1/4  pb-4 cursor-pointer transition-all duration-700 hover:bg-gray-600  hover:text-digiDefault`}
+        >
+          {' '}
+          <Link href='#'>
+            <a className='text-center block'>
+              <FontAwesomeIcon className={` mr-2  `} icon={faUsers} />
+              <span className='block'>Dependents</span>
+            </a>
+          </Link>
+        </div>
+        <div
+          className={`${
+            router.pathname.includes('/app/bookings')
+              ? selectedRouteStyle
+              : 'text-gray-400'
+          }   pt-4  w-1/4  pb-4 cursor-pointer transition-all duration-700 hover:bg-gray-600  hover:text-digiDefault`}
+        >
+          <Link href='/app/bookings'>
+            <a className=' block text-center'>
+              <FontAwesomeIcon className={` mr-2  `} icon={faClipboardList} />
+              <span className='block'>Bookings</span>
+            </a>
+          </Link>
+        </div>
+        {/* <div
+          className={`${
+            router.pathname == '/app/emergency'
+              ? selectedRouteStyle
+              : 'text-gray-400'
+          }   pt-4  w-1/4  pb-4 cursor-pointer transition-all duration-700 hover:bg-gray-600  hover:text-digiDefault`}
+        >
+          {' '}
+          <Link href='#'>
+            <a className=' block text-center'>
+              <FontAwesomeIcon className={` mr-2  `} icon={faTruckMedical} />
+              <span className='block '>Emergency</span>
+            </a>
+          </Link>
+        </div> */}
+        <div
+          className={`${
+            router.pathname == '/app/profile'
+              ? selectedRouteStyle
+              : 'text-gray-400'
+          }   pt-4  w-1/4  pb-4 cursor-pointer transition-all duration-700 hover:bg-gray-600  hover:text-digiDefault`}
+        >
+          {' '}
+          <Link href='#'>
+            <a className=' block text-center '>
+              <FontAwesomeIcon className={` mr-2  `} icon={faUser} />
+              <span className='block'>Profile</span>
+            </a>
+          </Link>
+        </div>
+        {/* <div
+          className={`${
+            router.pathname == '/app/profile'
+              ? selectedRouteStyle
+              : 'text-gray-400'
+          }   pt-4  w-1/4  pb-4 cursor-pointer transition-all duration-700 hover:bg-gray-600  hover:text-digiDefault`}
+        >
+          <button
+            className='block text-center w-full'
+            onClick={() => {
+              updateLoginDataDispatch(
+                updateLoginData({
+                  deviceToken: null,
+                  userId: null,
+                  loginToken: null,
+                  role: null,
+                  estate: null,
+                })
+              );
+            }}
+          >
+            <FontAwesomeIcon className={` mr-2  `} icon={faRightFromBracket} />
+            <span className='block'>Logout</span>
+          </button>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+export default MobileMenu;
