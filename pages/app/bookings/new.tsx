461b0d0ade72e531fde6c78b20ba74937b1dc800
Leon Chux <epicgenii18@gmail.com>
2022-12-08 18:05:13 +0100

    refactored new booking view

--- pages/app/bookings/new.tsx
+++ pages/app/bookings/new.tsx
@@ -9,7 +9,11 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import axiosErrorHandler from 'helpers/axiosErrorHandler';
 import { useSelector, useDispatch } from 'react-redux';
 import Link from 'next/link';
+import PreviousPage from 'components/navigation/previous_page/PreviousPage';
 
+import { Accordion, AccordionTab } from 'primereact/accordion';
+
+import { Checkbox } from 'primereact/checkbox';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import { updateToastData } from 'reducers/utility';
 import digiEstateAxiosInstance from 'helpers/digiEstateAxiosInstance';
@@ -41,7 +45,11 @@ const BookGuests = () => {
     setFormLoading(false);
   };
 
+  const [activeAccordionIndex, setActiveAccordionIndex] = useState<number>(0);
   const [formLoading, setFormLoading] = useState(false);
+
+  const [bookInVehicle, setBookInVehicle] = useState(false);
+  const [passExtraInstructions, setPassExtraInstructions] = useState(false);
   const phoneRegExp =
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
   const [retreivedToken, setRetrievedToken] = useState<boolean>(false);
@@ -169,18 +177,7 @@ const BookGuests = () => {
   return (
     <div className=' pt-10 pl-2 pr-2'>
       <div className=' '>
-        <div className='mb-6 text-xs'>
-          <Link href='/app/bookings'>
-            <a className='underline'>
-              <span className=''>
-                {' '}
-                <FontAwesomeIcon className={` mr-2 `} icon={faLeftLong} />
-                <span>Go Back</span>
-              </span>
-            </a>
-          </Link>
-        </div>
-        <h2 className='mb-8  lato-font'>Book Guests</h2>
+        <PreviousPage label='Book Guests' />
         <form
           className='mb-4  ml-auto mr-auto lg:pr-0 lg:pl-0 pl-2 pr-2 '
           onSubmit={handleSubmit(bookGuests)}
@@ -195,72 +192,15 @@ const BookGuests = () => {
               options={bookingTypes}
               onChange={(e) => setBookingMode(e.value)}
             ></SelectButton>
-            <div className='x2l:w-2/3 ml-auto mr-auto'>
-              <div className='mb-4 flex flex-col md:flex-row justify-between gap-y-2.5 md:gap-x-2.5 '>
-                <div className='w-full'>
-                  <label className='text-black'>
-                    Vehicle Make
-                    <input
-                      {...register('vehicle_make')}
-                      type='text'
-                      autoComplete='on'
-                      className='rei-text-input'
-                      placeholder='2005 Toyota Corolla'
-                    />
-                  </label>
-                  {errors['vehicle_make'] && (
-                    <ErrorMessage
-                      message={errors['vehicle_make']['message']!}
-                    />
-                  )}
-                </div>
-              </div>
-              <div className='mb-4 flex flex-col md:flex-row justify-between gap-y-2.5 md:gap-x-2.5 '>
-                <div className='w-full'>
-                  <label className='text-black'>
-                    Vehicle Plate Number
-                    <input
-                      {...register('vehicle_plate_number')}
-                      type='text'
-                      autoComplete='on'
-                      className='rei-text-input'
-                      name='plate_number'
-                    />
-                  </label>
-                  {errors['vehicle_plate_number'] && (
-                    <ErrorMessage
-                      message={errors['vehicle_plate_number']['message']!}
-                    />
-                  )}
-                </div>
-              </div>
-              <div className='mb-4 flex flex-col md:flex-row justify-between gap-y-2.5 md:gap-x-2.5 '>
-                <div className='w-full'>
-                  <label className='text-black'>
-                    Extra Instructions
-                    <textarea
-                      {...register('comment')}
-                      name=''
-                      className='rei-text-text-area '
-                      id=''
-                      rows={4}
-                    ></textarea>
-                  </label>
-                  {errors['comment'] && (
-                    <ErrorMessage message={errors['comment']['message']!} />
-                  )}
-                </div>
-              </div>
-            </div>
 
             {/* section start */}
 
             {/* Section End */}
 
-            <div className='text-center mb-4 '>
+            <div className='text-center mb-4 mt-2 '>
               <button
                 disabled={formLoading ? true : false}
-                className=' bg-gray-600 text-digiDefault flex ml-auto mr-auto items-center text-sm  mb-4 rounded-lg pl-8 pr-8 pt-2 pb-2'
+                className='  border-2 border-gray-600 text-gray-600 flex ml-auto mr-auto items-center text-sm  mb-4 rounded-lg pl-8 pr-8 pt-2 pb-2'
                 type='button'
                 onClick={() => {
                   setShowBookGuestsModal(true);
@@ -269,7 +209,7 @@ const BookGuests = () => {
                 <span>Add Guests</span>
               </button>
             </div>
-            <div className='guests-container'>
+            <div className='guests-container mb-10'>
               {guests.length < 1 && (
                 <div className='bg-gray-600 mb-2 text-digiDefault text-center text-sm pt-2 pb-2'>
                   <p>No guests have been added yet</p>
@@ -317,11 +257,82 @@ const BookGuests = () => {
                 );
               })}
             </div>
-            <div>
-              <button className='bg-gray-600 pt-2 pb-2 pl-4 pr-4 rounded-lg text-digiDefault'>
-                Submit Booking
-              </button>
+            <div className='mb-10'>
+              <Accordion className='text-sm'>
+                <AccordionTab
+                  className='text-sm lato-font'
+                  header='Include Vehicle'
+                >
+                  <div className='x2l:w-2/3 ml-auto mr-auto pt-4'>
+                    <div className='mb-4 flex flex-col md:flex-row justify-between gap-y-2.5 md:gap-x-2.5 '>
+                      <div className='w-full mb-4'>
+                        <label className='text-black text-sm'>
+                          Vehicle Make{' '}
+                        </label>
+                        <input
+                          {...register('vehicle_make')}
+                          type='text'
+                          autoComplete='on'
+                          className='rei-text-input'
+                          placeholder='2005 Toyota Corolla'
+                        />
+
+                        {errors['vehicle_make'] && (
+                          <ErrorMessage
+                            message={errors['vehicle_make']['message']!}
+                          />
+                        )}
+                      </div>
+                    </div>
+                    <div className='mb-4 flex flex-col md:flex-row justify-between gap-y-2.5 md:gap-x-2.5 '>
+                      <div className='w-full'>
+                        <label className='text-black'>
+                          Vehicle Plate Number
+                          <input
+                            {...register('vehicle_plate_number')}
+                            type='text'
+                            autoComplete='on'
+                            className='rei-text-input'
+                            name='plate_number'
+                          />
+                        </label>
+                        {errors['vehicle_plate_number'] && (
+                          <ErrorMessage
+                            message={errors['vehicle_plate_number']['message']!}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </AccordionTab>
+                <AccordionTab className='text-sm' header='Extra Instructions'>
+                  <div className='w-full pt-2 pb-2'>
+                    <label className='text-black'>
+                      Extra Instructions
+                      <textarea
+                        {...register('comment')}
+                        name=''
+                        className='rei-text-text-area '
+                        id=''
+                        rows={4}
+                      ></textarea>
+                    </label>
+                    {errors['comment'] && (
+                      <ErrorMessage message={errors['comment']['message']!} />
+                    )}
+                  </div>
+                </AccordionTab>
+              </Accordion>
+
+              {passExtraInstructions && <div className='mb-4  j'></div>}
             </div>
+            {guests.length > 0 && (
+              <div className='text-center'>
+                <button className='bg-gray-600 text-sm pt-2 pb-2 pl-4 pr-4 rounded-lg text-digiDefault'>
+                  Submit Booking
+                </button>
+              </div>
+            )}
           </div>
         </form>
         <Dialog
@@ -466,48 +477,67 @@ const BookGuests = () => {
           </div>
         </Dialog>
       </div>
-      <style global jsx>{`
-        @keyframes p-progress-spinner-color {
-          100%,
-          0% {
-            stroke: #000;
+      <style global jsx>
+        {`
+          @keyframes p-progress-spinner-color {
+            100%,
+            0% {
+              stroke: #000;
+            }
+            40% {
+              stroke: #0057e7;
+            }
+            66% {
+              stroke: #008744;
+            }
+            80%,
+            90% {
+              stroke: #f000;
+            }
           }
-          40% {
-            stroke: #0057e7;
+          #bookingMode > div {
+            height: 2rem !important;
+            font-family: 'Lato', sans-serif;
+            font-weight: normal;
           }
-          66% {
-            stroke: #008744;
+          #bookingMode .p-button-label {
+            font-weight: normal !important;
+            font-size: 0.8rem;
           }
-          80%,
-          90% {
-            stroke: #f000;
+          #bookingMode .p-button.p-highlight {
+            background: #4b5563;
+            color: #fff2d9;
           }
-        }
-        #bookingMode > div {
-          height: 2rem !important;
-          font-family: 'Lato', sans-serif;
-          font-weight: normal;
-        }
-        #bookingMode .p-button-label {
-          font-weight: normal !important;
-          font-size: 0.8rem;
-        }
-        #bookingMode .p-button.p-highlight {
-          background: #4b5563;
-          color: #fff2d9;
-        }
-        #bookingMode {
-          text-align: right;
-        }
+          #bookingMode {
+            text-align: right;
+          }
 
-        .guests-container {
-          min-height: 10rem;
-        }
+          #bookGuestsDialog {
+            margin: 0;
+          }
 
-        #bookGuestsDialog {
-          margin: 0;
-        }
-      `}</style>
+          .p-checkbox .p-checkbox-box.p-highlight {
+            --tw-bg-opacity: 1;
+            background-color: rgb(75 85 99 / var(--tw-bg-opacity));
+            border-color: rgb(75 85 99 / var(--tw-bg-opacity));
+            background: rgb(75 85 99 / var(--tw-bg-opacity));
+          }
+          .p-checkbox .p-checkbox-box.p-highlight:hover {
+            --tw-bg-opacity: 1;
+            background-color: rgb(75 85 99 / var(--tw-bg-opacity));
+            border-color: rgb(75 85 99 / var(--tw-bg-opacity));
+            background: rgb(75 85 99 / var(--tw-bg-opacity));
+          }
+
+          .p-checkbox:not(.p-checkbox-disabled) .p-checkbox-box.p-focus {
+            border-color: rgb(75 85 99 / var(--tw-bg-opacity)) !important;
+          }
+          .p-checkbox:not(.p-checkbox-disabled)
+            .p-checkbox-box.p-highlight:hover {
+            background: rgb(75 85 99 / var(--tw-bg-opacity));
+          }
+        `}
+      </style>
     </div>
   );
 };
