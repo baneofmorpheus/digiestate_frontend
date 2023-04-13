5be797be2d4d92957add2f7f708ad31c444cf912
Leon Chux <epicgenii18@gmail.com>
2023-04-13 14:25:44 +0100

    added admin reducer

--- reducers/index.ts
+++ reducers/index.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import authenticationReducer from './authentication';
 import utilityReducer from './utility';
+import adminReducer from './admin';
 
 const rootReducer = combineReducers({
   authentication: authenticationReducer,
   utility: utilityReducer,
+  admin: adminReducer,
 });
 
 export default rootReducer;
